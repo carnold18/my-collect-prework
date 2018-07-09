f8dec96053337faa4f3a3c2361244e4ca5ec32a6
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-09 12:50:05 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,10 @@
 def my_collect(array)
   if block_given?
     i = 0
- 
     while i < array.length
       yield(array[i].upcase && array[i].capitalize)
       i = i + 1
     end
- 
     array
   else
     nil
